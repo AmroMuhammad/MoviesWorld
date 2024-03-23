84717701b5c6cf9d07941c818756b3a0506f0c14
Amr Muhammad <amrmu94@gmail.com>
2024-03-23 15:03:36 +0200

    init commit

---
+++ MoviesWorld/MoviesWorld/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  MoviesWorld
+//
+//  Created by Amr Muhammad on 23/03/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ MoviesWorld/MoviesWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoviesWorld
+//
+//  Created by Amr Muhammad on 23/03/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
